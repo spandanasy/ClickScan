266d707d17713961718fd459b5d1b7418b55925e
unknown <spandanasy21@gmail.com>
2024-09-12 14:24:39 +0530

    dashboard changes

--- force-app/main/default/lwc/dashboard/dashboard.js
+++ force-app/main/default/lwc/dashboard/dashboard.js
@@ -1,33 +1,34 @@
 import { LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import logo from '@salesforce/resourceUrl/clickScan';
 import getOrgAndUserInfo from '@salesforce/apex/OrganizationUserController.getOrgAndUserInfo';
 
-export default class DashboardComponent extends LightningElement {
+export default class DashboardComponent extends NavigationMixin(LightningElement) {
+ 
     orgName;
     orgId;
     userName;
     allUserNames = [];
+    drawerCount = 16;
+    folderCount = 92;
+    userCount = 22;
+    logoUrl = logo;
 
     @wire(getOrgAndUserInfo)
-    wiredOrgAndUserInfo({ error, data }) {
-        if (data) {
-            this.orgName = data.orgName;
-            this.orgId = data.orgId;
-            this.userName = data.userName;
-            this.allUserNames = data.allUserNames; 
-        } else if (error) {
-            console.error('Error retrieving organization and user info', error);
+    wiredOrgAndUserInfo(result) {
+        if (result.data) {
+            this.orgName = result.data.orgName;
+            this.orgId = result.data.orgId;
+            this.userName = result.data.userName;
+            this.allUserNames = result.data.allUserNames; 
+        } else if (result.error) {
+            console.error('Error retrieving organization and user info', result.error);
         }
     }
 
     get cardTitle() {
         return `${this.orgName} Org ID: ${this.orgId} User Name: ${this.userName}`;
     }
-
-    drawerCount = 16;
-    folderCount = 92;
-    userCount = 22;
-    logoUrl = logo;
 
     handleCreateDrawer() {
         // Logic to create a new drawer
@@ -43,5 +44,24 @@ export default class DashboardComponent extends LightningElement {
 
     handleGenerateReport() {
         // Logic to generate an audit report
+    }
+
+    navigateToDrawerManagement() 
+    {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: 'https://terralogic3-dev-ed.develop.lightning.force.com/lightning/n/Drawer_Management' // Ensure this matches the component's name
+            }
+        });  
+    }
+    navigateToUserManagement() 
+    {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: 'https://terralogic3-dev-ed.develop.lightning.force.com/lightning/n/User_Management' // Ensure this matches the component's name
+            }
+        });
     }
 }
---
+++ force-app/main/default/lwc/drawer_management/drawer_management.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class DrawerManagementPage extends LightningElement {
+    // This is a simple component, so no additional logic is needed
+}
